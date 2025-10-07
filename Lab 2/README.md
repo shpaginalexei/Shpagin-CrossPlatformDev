952d41c29cd689134356234364f9849822c48e50
shpaginalexei <shpagin05@gmail.com>
2025-10-07 13:39:47 +0300

    (lab2) Init Project

---
+++ Lab 2/README.md
@@ -0,0 +1,7 @@
+# Разработка серверной части
+
+> Разработка приложения Web API (фреймворк.NET)
+
+
+Туториал: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-9.0&tabs=visual-studio-code
+
---
+++ Lab 2/ShpaginWebApi/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
