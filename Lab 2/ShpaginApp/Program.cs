ba87b2375605005f31b5c14ae1ee67dc8ba3140f
shpaginalexei <shpagin05@gmail.com>
2025-10-22 12:35:39 +0300

    Rename project

--- Lab 2/ShpaginApp/Program.cs
+++ Lab 2/ShpaginApp/Program.cs
@@ -8,11 +8,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwaggerUi(options =>
-    {
-        options.DocumentPath = "/openapi/v1.json";
-    });
+  app.MapOpenApi();
+  app.UseSwaggerUi(options =>
+  {
+    options.DocumentPath = "/openapi/v1.json";
+  });
 }
 
 app.UseHttpsRedirection();
